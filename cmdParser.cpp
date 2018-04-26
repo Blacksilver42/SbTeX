e1dfdbad8084d776241040eabf46da21a11ed8d4
Connor [REDACTED] <blacksilverck35@gmail.com>
2018-04-26 12:59:06 +0000

    Optimized getInfo - only one for loop

--- cmdParser.cpp
+++ cmdParser.cpp
@@ -11,10 +11,13 @@ int getInfo(int argc, char* argv[], Info &info){
 	
 	info.arg0 = argv[0];
 	
-	// Parse for -o <OUTFILE>
+	// Parse for -o <OUTFILE> and -f <FONT>
 	int i;
 	info.outfile = stdout;
 	info.outfile_name = "stdout";
+	info.font = DEFAULT_FONT;
+	info.input = NULL;
+	
 	for(i=0;i<argc;i++){
 		if(strcmp(argv[i],"-o") == 0){
 			info.outfile = fopen(argv[i+1], "w");
@@ -25,26 +28,17 @@ int getInfo(int argc, char* argv[], Info &info){
 				return 1;
 			}
 		}
-	}
-	
-	// Parse for -f <FONT>
-	info.font = DEFAULT_FONT;
-	for(i=0;i<argc;i++){
 		if((strcmp(argv[i],"-f") == 0) || strcmp(argv[i],"--font") == 0){
 			info.font = argv[i+1];
-			break;
 		}
-	}
-	sprintf(info.fontpath, "fonts/%s/",info.font);
-	
-	// Parse for -i <INPUT>
-	info.input = NULL;
-	
-	for(i=0;i<argc;i++){
+		
 		if(strcmp(argv[i],"-i") == 0){
 			info.input = argv[i+1];
 		}
 	}
+	
+	sprintf(info.fontpath, "fonts/%s/",info.font);
+	
 	if(info.input == NULL){
 		errno = EINVAL;
 		fprintf(stderr, "%s: %s\n", info.arg0, strerror(errno));
